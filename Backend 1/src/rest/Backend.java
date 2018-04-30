9595dff96dbd1115e7d1236ca2323ec55172b929
rallerG <raller799@live.dk>
2018-04-30 14:52:39 +0200

    added return string

--- Backend 1/src/rest/Backend.java
+++ Backend 1/src/rest/Backend.java
@@ -5,33 +5,37 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import DTO.Personer;
 
 @Path("/backend")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class Backend {
 
 	@POST
 	@Path("/create")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void createUser(
+	public String createUser(
 			@FormParam("userId") int userId,
 			@FormParam("userName") String userName,
 			@FormParam("ini") String ini,
 			@FormParam("cpr") String cpr,
 			@FormParam("password") String password,
-			@FormParam("roles") List <String> roles
-			) {
+			@FormParam("roles") List <String> roles) {
+		
 		//TODO add to list
 		Personer test = new Personer(userId, userName, ini, cpr, password, roles);
 		System.out.println(test.toString());
 		
+		String result = "It works, maybe";
 		
 		//Old
 		//String result = "The ingredient added has id-number: " + id + " and the name " + name +
 		//		". The amount added was " + amount + ".";
-		//return result;
+		return result;
 	}
 	
 	//Notes
