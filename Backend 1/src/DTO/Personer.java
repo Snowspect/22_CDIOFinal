6176f7f37b21c4e98a4401bb1aa51e44fc5cfae4
rallerG <raller799@live.dk>
2018-04-30 13:21:18 +0200

    DTO

---
+++ Backend 1/src/DTO/Personer.java
@@ -0,0 +1,73 @@
+package DTO;
+
+import java.util.List;
+
+public class Personer {
+	int userId;
+	String userName;
+	String ini;
+	String cpr;
+	String password;
+	List <String> roles;
+	
+	//Stig sagde der også skulle være en tom constructor
+	public Personer() {
+	}
+
+	public Personer(int userId, String userName, String ini, String cpr, String password, List <String> roles) {
+		this.userId = userId;
+		this.userName = userName;
+		this.ini = ini;
+		this.cpr = cpr;
+		this.password = password;
+		this.roles = roles;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getIni() {
+		return ini;
+	}
+
+	public void setIni(String ini) {
+		this.ini = ini;
+	}
+
+	public String getCpr() {
+		return cpr;
+	}
+
+	public void setCpr(String cpr) {
+		this.cpr = cpr;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+}
