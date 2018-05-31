aa5bff8e5cc40a5332261b16b82d16b81b6dcd7a
Cilian <32563603+Cilian@users.noreply.github.com>
2018-05-31 12:54:55 +0200

    rest

--- Backend 1/src/user/StockResources.java
+++ Backend 1/src/user/StockResources.java
@@ -1,6 +1,6 @@
 package user;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -12,14 +12,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import DTO.Raavare;
-import DTO.Recept;
 
 @Path("/stock")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class StockResources {
 	private static ArrayList <Raavare> ravareList = new ArrayList<>();
-	private static ArrayList <Recept> receptList = new ArrayList<>();
+
 
 	//*** Ravare ***//
 	//PUT
@@ -74,44 +73,7 @@ public class StockResources {
 	
 	//*** Recept ***//
 	//PUT 
-	@PUT
-	@Path("{receptNr}")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String submit(Recept rec) {
-		receptList.add(rec);
-		
-		System.out.println("Created user: " + rec.toString());
-		System.out.println("Current list " + receptList.toString());
-		
-		String result = "created recept";
-		return result;
-	}
-	
-	//GET
-	@GET
-	@Path("")
-	@Produces(MediaType.APPLICATION_JSON)
-	public ArrayList<Recept> getRecept()
-	{
-		return receptList;
-	}
-	
-	//POST
-//	@POST
-//	@Path("{receptNr}")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	public String update(Recept rec) {
-//		
-//		for (Recept recpt : receptList) {
-//			if(recpt.getReceptId() == rec.getReceptId())
-//			{
-//				recpt.setReceptId(rec.getReceptId());
-//				recpt.setName(rec.getName());
-//				recpt.setSupplier(rec.getSupplier());
-//			}
-//		}			
-//		return "Updated råvare";
-//	}
+
 	
 	//DELETE
 	
