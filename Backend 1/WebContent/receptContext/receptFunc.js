5c927150f7cc3f4692d1e2125e955c9be3f3bab3
rallerG <raller799@live.dk>
2018-06-01 11:58:29 +0200

    Fixed form

--- Backend 1/WebContent/receptContext/receptFunc.js
+++ Backend 1/WebContent/receptContext/receptFunc.js
@@ -6,17 +6,23 @@ function generateRaavare() {
 	var number = 0;
 	number = document.getElementById("raavareCount").value;
 	
-	var form = $("#test").html();
-	
-	
 	for(i = 0; i < number; i++) {
-//		debugger;
-		//TODO
-		//Append HTML to div
-//		form.id = i;
+		var form = '<br>'+
+		'				<center>RaavareID</center>'+
+		'				<center>'+
+		'					<input type="number" id="raavareID' + i + '" required pattern="[0-9.]+">'+
+		'				</center>'+
+		'				<br>'+
+		'				<center>NomNetto i kg</center>'+
+		'				<center>'+
+		'					<input type="number" id="nomNetto' + i + '" step="0.01" required pattern="[0-9.]+">'+
+		'				</center>'+
+		'				<br>'+
+		'				<center>Tolerance i %</center>'+
+		'				<center>'+
+		'					<input type="number" id="tolerance' + i + '" step="0.01" required pattern="[0-9.]+">'+
+		'				</center>';
+			
 		$("#numberOfRaavare").append(form);
-		
-//		document.getElementById("numberOfRaavare").append(form);
-		console.log("x");
 	}
 }
