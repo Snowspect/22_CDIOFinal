9cfa0c6b93b73fe2b671fea001654201b63059b5
Cilian <32563603+Cilian@users.noreply.github.com>
2018-06-01 22:38:41 +0200

    produkt

--- Backend 1/WebContent/ProduktbatchContext/Produktbatch.js
+++ Backend 1/WebContent/ProduktbatchContext/Produktbatch.js
@@ -1,8 +1,66 @@
+/**
+ * Generate n input fields in form depending on number of needed raavare
+ */
+function generateProdukt() {
+	var number = 0;
+	number = document.getElementById("produktCount").value;
+	
+	for(i = 0; i < number; i++) {
+		var form = '	<tr>' + 
+		'				<td><center>RaavareID</center>'+
+		'				<center>'+
+		'					<input type="number" id="raavareID' + i + '" required pattern="[0-9.]+">'+
+		'				</center></td>'+
+		'				<br>'+
+		'				<td><center>NomNetto i kg</center>'+
+		'				<center>'+
+		'					<input type="number" id="nomNetto' + i + '" step="0.01" required pattern="[0-9.]+">'+
+		'				</center></td>'+
+		'				<br>'+
+		'				<td><center>Tolerance i %</center>'+
+		'				<center>'+
+		'					<input type="number" id="tolerance' + i + '" step="0.01" required pattern="[0-9.]+">'+
+		'				</center></td>' +
+		'				</tr>' +
+		'				<td><center>Tolerance i %</center>'+
+		'				<center>'+
+		'					<input type="number" id="tolerance' + i + '" step="0.01" required pattern="[0-9.]+">'+
+		'				</center></td>' +
+		'				</tr>';
+		
+			
+		$("#numberOfRaavare").append(form);
+	}
+}
+
+function getDataFromHTML() {
+	var receptId = document.getElementById("receptID").value;
+	var receptNavn = document.getElementById("receptNavn").value
+	
+	var recept = {
+		receptId : receptId,
+		receptNavn : receptNavn,
+		ingrediens : []		
+	};
+	
+	
+	$("tr").each(function(index,element){
+		//debugger;
+		var ravaareId = document.getElementById("raavareID" + index).value;
+		var nomNetto = document.getElementById("nomNetto" + index).value;
+		var tolerance = document.getElementById("tolerance" + index).value;
+		var obj = {raavareId : ravaareId, nomNetto: nomNetto, tolerance: tolerance};
+		recept.ingrediens.push(obj);
+	});
+	return recept;
+}
+
+
 
 function submit() { //Formen kalder denne function, sikre at alle felter er udfyldt
 	myJSON = getProduktFromHTML(); //myJSON is an object just like "bruger"
 	$.ajax({ //Indleder et asynkront ajax kald
-		url : "cargostock/produktbatch/", //specificerer endpointet
+		url : "../cargostock/produktbatch/", //specificerer endpointet
 		type : 'POST', //Typen af HTTP requestet
 		data : 	JSON.stringify(myJSON),
 		contentType : 'application/json',
