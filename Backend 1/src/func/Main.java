021db10bef85aebe61c167d7b682348b11997989
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-06-04 12:13:21 +0200

    Added main

---
+++ Backend 1/src/func/Main.java
@@ -0,0 +1,23 @@
+package func;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+
+import DTO.Afvejning;
+import DTO.Personer;
+import DTO.RaavareBatch;
+
+public class Main {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		// TODO Auto-generated method stub
+		Afvejning dto = new Afvejning();
+		Personer pers = new Personer();
+		RaavareBatch RaaBa = new RaavareBatch();
+//		TCPSocket tcp = new TCPSocket();
+		Weight_IO io = new Weight_IO(dto);
+		io.run();
+	}
+
+}
+
