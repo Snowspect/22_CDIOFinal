66aec22534aa10edf0a77fafa059b918897eec33
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-06-11 12:46:33 +0200

    Cleanup
    Co-Authored-By: cilian <cilian@users.noreply.github.com>

--- Backend 1/src/func/Weight_IO.java
+++ Backend 1/src/func/Weight_IO.java
@@ -298,25 +298,3 @@ public class Weight_IO {
 
 	}
 }
-		String getRecept = "Select recept_navn from produktbatch NATURAL JOIN recept where pb_id = ? group by recept_navn";
-
-
-		try {
-			getReceptName = sqlCon.prepareStatement(getRecept);
-
-			getReceptName.setInt(1, id);
-			rs = getReceptName.executeQuery();
-			if(rs.first()) {
-				recept = rs.getString("recept_navn");	
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			if(getReceptName != null) {
-				getReceptName.close();
-			}
-		}
-		return recept;
-	}
-}
