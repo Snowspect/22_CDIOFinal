ff874ad6f21a8136731e95ffa5ce1a474e885f1e
Cilian <32563603+Cilian@users.noreply.github.com>
2018-06-12 12:53:28 +0200

    Status DAO
    Co-Authored-By: Thomas Mattsson <tmattsson0@users.noreply.github.com>

--- Backend 1/src/daointerfaces01917/StatusDAO.java
+++ Backend 1/src/daointerfaces01917/StatusDAO.java
@@ -1,12 +1,7 @@
 package daointerfaces01917;
 
 import java.sql.SQLException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Arrays;
-import JDBC.Connector;
+
 
 public interface StatusDAO {
 
