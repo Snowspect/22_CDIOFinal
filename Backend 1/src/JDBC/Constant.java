6420ca929e6d16da37714e97514d12433163e860
Snowspect <s155384@student.dtu.dk>
2018-06-13 13:54:20 +0200

    switch of variablename

--- Backend 1/src/JDBC/Constant.java
+++ Backend 1/src/JDBC/Constant.java
@@ -6,10 +6,10 @@ package JDBC;
 public abstract class Constant
 {
 	public static final String
-		server					= "localhost",  // database-serveren
-		database				=  "project",  //"jdbcdatabase", // navnet paa din database = dit studienummer
-		username				= "root", // dit brugernavn = dit studienummer 
-		password				= ""; // dit password som du har valgt til din database
+		server					= "diplomportal.c2nouactg6m6.eu-west-1.rds.amazonaws.com",  // database-serveren
+		database				=  "s155384",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		username				= "s155384", // dit brugernavn = dit studienummer 
+		password				= "6x6yPWuu4lS81QIdatabasestatus"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
