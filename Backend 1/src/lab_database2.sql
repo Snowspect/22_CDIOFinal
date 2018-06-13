ece5c95d5aa5b4c7bb22c2eaf2d740b68dcc0b7e
Thomas Mattsson <32563069+Tmattsson0@users.noreply.github.com>
2018-06-13 13:49:04 +0200

    Changed SQL to medicin names

--- Backend 1/src/lab_database2.sql
+++ Backend 1/src/lab_database2.sql
@@ -41,26 +41,44 @@ CREATE TABLE produktbatchkomponent(pb_id INT, rb_id INT, tara REAL, netto REAL, 
 INSERT INTO personer(cpr, opr_navn, ini) VALUES
 ('070770-7007', 'Angelo A', 'AA' ),
 ('080880-8008', 'Antonella B', 'AB'),
-('090990-9009', 'Luigi C', 'LC');
+('090990-9009', 'Luigi C', 'LC'),
+('030330-3003', 'Walter W', 'WW'),
+('040440-4004', 'Harry P', 'HP'),
+('050550-5005', 'Arnold S', 'AS'),
+('060660-6006', 'Pablo E', 'PE'),
+('020220-2002', 'Donald T', 'DT'),
+('010110-1001', 'Jesse P', 'JE');
 
 INSERT INTO roller(rolle_id, cpr, rolle) VALUES
-(1, '070770-7007', 'Admin' ),
+(1, '070770-7007', 'Admin'),
 (2, '080880-8008', 'Laborant'),
-(3, '090990-9009', 'Foreman');
+(3, '090990-9009', 'Foreman'),
+(4, '030330-3003', 'Admin'),
+(5, '040440-4004', 'Laborant'),
+(6, '050550-5005', 'Laborant'),
+(7, '060660-6006', 'Foreman'),
+(8, '020220-2002', 'Laborant'),
+(9, '010110-1001', 'Foreman');
 
 INSERT INTO operatoer(rolle_id, opr_status) VALUES
 (1, 1),
 (2, 1),
-(3, 0);
+(3, 0),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 1);
 
 INSERT INTO raavare(raavare_id, raavare_navn, leverandoer) VALUES
-(1, 'dej', 'Wawelka'),
-(2, 'tomat', 'Knoor'),
-(3, 'tomat', 'Veaubais'),
-(4, 'tomat', 'Franz'),
-(5, 'ost', 'Ost og Skinke A/S'),
-(6, 'skinke', 'Ost og Skinke A/S'),
-(7, 'champignon', 'Igloo Frostvarer');
+(1, 'Ibuprofen', 'Big Pharma'),
+(2, 'Aluminium', 'Novo Nordisk'),
+(3, 'Codein', 'Novo Nordisk'),
+(4, 'Kulsyre', 'Matas'),
+(5, 'Salmiak', 'Apoteket A/S'),
+(6, 'Saltsyre', 'Medifirma A/S'),
+(7, 'Salt', 'Salt Inc.');
 
 INSERT INTO raavarebatch(rb_id, raavare_id, maengde) VALUES
 (1, 1, 1000),
@@ -72,33 +90,51 @@ INSERT INTO raavarebatch(rb_id, raavare_id, maengde) VALUES
 (7, 7, 100);
 
 INSERT INTO recept(recept_id, recept_navn) VALUES
-(1, 'margherita'),
-(2, 'prosciutto'),
-(3, 'capricciosa');
+(1, 'Pinex'),
+(2, 'Ipren'),
+(3, 'Kodimagnyl'),
+(4, 'Panodil'),
+(5, 'Aspirin'),
+(6, 'Morfin');
 
 
 INSERT INTO receptkomponent(recept_id, raavare_id, nom_netto, tolerance) VALUES
-(1, 1, 10.0, 0.1),
+(1, 1, 4.0, 0.1),
 (1, 2, 2.0, 0.1),
 (1, 5, 2.0, 0.1),
 
-(2, 1, 10.0, 0.1),
+(2, 1, 2.0, 0.1),
 (2, 3, 2.0, 0.1),  
 (2, 5, 1.5, 0.1),
 (2, 6, 1.5, 0.1),
 
-(3, 1, 10.0, 0.1),
+(3, 1, 5.0, 0.1),
 (3, 4, 1.5, 0.1),
 (3, 5, 1.5, 0.1),
 (3, 6, 1.0, 0.1),
-(3, 7, 1.0, 0.1);
+(3, 7, 1.0, 0.1),
+
+(4, 1, 2.5, 0.1),
+(4, 3, 2.0, 0.1),  
+(4, 7, 1.5, 0.1),
+(4, 2, 3.5, 0.1),
+
+(5, 1, 1.0, 0.1),
+(5, 5, 5.0, 0.1),  
+(5, 7, 1.5, 0.1),
+
+(6, 2, 4.0, 0.1),
+(6, 7, 2.0, 0.1); 
 
 INSERT INTO produktbatch(pb_id, recept_id, status) VALUES
 (1, 1, 2), 
 (2, 1, 2),
 (3, 2, 2),
 (4, 3, 1),
-(5, 3, 0);
+(5, 3, 0),
+(6, 1, 0),
+(7, 3, 0),
+(8, 2, 0);
 
 
 INSERT INTO produktbatchkomponent(pb_id, rb_id, tara, netto, rolle_id) VALUES
