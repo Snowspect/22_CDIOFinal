fc8ef5f7c4458268049d9604f7a87305e2e050f2
Snowspect <s155384@student.dtu.dk>
2018-06-13 14:09:36 +0200

    changed database connection

--- Backend 1/src/JDBC/Constant.java
+++ Backend 1/src/JDBC/Constant.java
@@ -7,9 +7,9 @@ public abstract class Constant
 {
 	public static final String
 		server					= "diplomportal.c2nouactg6m6.eu-west-1.rds.amazonaws.com",  // database-serveren
-		database				=  "s155384",  //"jdbcdatabase", // navnet paa din database = dit studienummer
+		database				= "s155384",  //"jdbcdatabase", // navnet paa din database = dit studienummer
 		username				= "s155384", // dit brugernavn = dit studienummer 
-		password				= "6x6yPWuu4lS81QIdatabasestatus"; // dit password som du har valgt til din database
+		password				= "6x6yPWuu4lS81QI"; // dit password som du har valgt til din database
 	
 	public static final int
 		port					= 3306;
